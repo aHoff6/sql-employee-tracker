acdea4998e7fd5af1a2d3fdbf09b158dbb3a33a1
ManHoff <achoffman91@gmail.com>
2023-02-07 13:00:57 -0500

    add prompts for showing data and ading emp/rol/dpt

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,35 @@
 DROP DATABASE IF EXISTS employee_database;
+-- DROP TABLE IF EXISTS department;
 CREATE DATABASE employee_database;
-
 USE employee_database;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name varchar(30) NOT NULL
+    id INT NOT NULL PRIMARY KEY auto_increment,
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary decimal not null,
-  department_id INT
+CREATE TABLE roles (
+    id INT NOT NULL PRIMARY KEY auto_increment,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
-CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-   role_id INT NOT NULL,
-  manager_id INT 
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY auto_increment,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
+
+
+
+
--- index.js
+++ index.js
@@ -56,18 +56,105 @@ const init = () => {
 
 init();
 
-const showDepartments = () => {
-     console.log("testing show department")
-   
-};
 
-const showRoles = () => {
-    console.log("testing show roles")
+const displayData = (query, args, successMessage) => {
+    db.query(query, args, (err, results) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.table(results);
+        console.log(successMessage);
+      }
+
+      // starts prompts over again
+      init();
+    });
+  };
   
-};
-
-const showEmployees = () => {
-    console.log("testing show employees")
+  // gets all department data and executes display data making it viewable in console as a table
+  const showDepartments = () => {
+    displayData(`SELECT * FROM department`, [], "Department data retrieved successfully.");
+  };
   
-};
-
+  // gets all roles data and executes display data making it viewable in console as a table
+  const showRoles = () => {
+    displayData(`SELECT * FROM roles`, [], "Role data retrieved successfully.");
+  };
+  
+  // gets all employee data and executes display data making it viewable in console as a table
+  const showEmployees = () => {
+    displayData(`SELECT * FROM employee`, [], "Employee data retrieved successfully.");
+  };
+  
+  // adding department prompts
+  const addDepartment = () => {
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the name of the department you'd like to add?",
+        name: "departmentName",
+      },
+    ]).then((ans) => {
+      displayData(
+        `INSERT INTO department(name) VALUES(?)`,
+        [ans.departmentName],
+        "Department added successfully."
+      );
+    });
+  };
+  
+  // adding role prompts
+  const addRole = () => {
+    db.promise()
+      .query(`SELECT id, name FROM department`)
+      .then((rows) => {
+        const deptChoices = rows[0].map(obj => obj.name);
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "What is the title of the role you'd like to add?",
+            name: "roleTitle",
+          },
+          {
+            type: "input",
+            message: "What is the salary for this role?",
+            name: "roleSalary",
+          },
+          {
+            type: "list",
+            message: "Which department is this role in?",
+            name: "department",
+            choices: deptChoices,
+          },
+        ]).then((ans) => {
+          const department = rows[0].find(d => d.name === ans.department);
+          displayData(
+            `INSERT INTO roles(title, salary, department_id) VALUES(?, ?, ?)`,
+            [ans.roleTitle, ans.roleSalary, department.id],
+            "Role added successfully."
+          );
+        });
+      });
+  };
+  
+  // adding employee prompts
+  const addEmployee = () => {
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the employee's first name?",
+        name: "firstName",
+      },
+      {
+        type: "input",
+        message: "What is the employee's last name?",
+        name: "lastName",
+      },
+    ]).then((ans) => {
+      displayData(
+        `INSERT INTO employee(first_name, last_name) VALUES(?, ?)`,
+        [ans.firstName, ans.lastName],
+        "Employee added successfully."
+      );
+    });
+  };
